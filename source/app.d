7025f459a98e260496f21c285dac8d880f2208d1
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-11-05 23:24:20 -0500

    tidy up code, make it work

--- source/app.d
+++ source/app.d
@@ -1,11 +1,21 @@
 import std.stdio;
-import dunit;
+/* import dunit; */
+import value;
 
 void main(string[] args) {
     version(unittest) {
-        dunit_main(args);
+        /* writeln("testing");
+        dunit_main(args); */
     }
     else {
-        writeln("todo: application");
+        DatacheValue x, y, sum, sumCheck;
+        x = new DatacheNumber(123);
+        y = new DatacheNumber(42);
+        sumCheck = new DatacheNumber(165);
+        sum = x.add(y);
+
+        writeln(sum, ";", sumCheck);
+        writeln(sum == sumCheck);
+        writeln(sum == new DatacheNumber(166));
     }
 }
