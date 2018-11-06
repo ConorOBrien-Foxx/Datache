70059641a95f0963c63549a8859633a893d252ea
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-11-05 23:47:46 -0500

    sort out default documentation

--- source/value/number.d
+++ source/value/number.d
@@ -6,18 +6,24 @@ import std.traits;
 
 import value;
 
+/// The basic numeric type in Datache.
 class DatacheNumber : DatacheValue {
     private static const string TAG = "number";
+    /// The atomic value of a `DatacheNumber` is a `BigInt`.
     public BigInt value;
 
+    /// Converts input to `BigInt`, if possible automatically.
     this(T)(T x) {
         this(BigInt(x));
     }
+    /// Basic constructor.
     this(BigInt n) {
         super(DatacheNumber.TAG);
         value = n;
     }
 
+    /// Numbers are only equal to other numbers, and only if they share the
+    /// same value.
     override bool instanceEqualTo(DatacheValue other) {
         import std.stdio;
 
@@ -28,6 +34,8 @@ class DatacheNumber : DatacheValue {
 
         return value == a.value;
     }
+    
+    ///
     unittest {
         DatacheNumber a1, a2, b;
         a1 = new DatacheNumber(100);
@@ -39,6 +47,9 @@ class DatacheNumber : DatacheValue {
         assert(!b.instanceEqualTo(a1));
     }
 
+    /// Adds two `DatacheValue` only when they are both numbers.
+    /// Otherwise, returns `null`.
+    // TODO: design better return behaviour
     override DatacheValue add(DatacheValue other) {
         if(other.type == DatacheNumber.TAG) {
             DatacheNumber a = cast(DatacheNumber)other;
@@ -47,6 +58,7 @@ class DatacheNumber : DatacheValue {
         return null;
     }
 
+    ///
     unittest {
         DatacheValue a = new DatacheNumber(100);
         DatacheValue b = new DatacheNumber(42);
@@ -54,6 +66,7 @@ class DatacheNumber : DatacheValue {
         assert(a.add(b) == new DatacheNumber(142));
     }
 
+    /// Returns the string representation of `value`.
     override string toString() {
         return to!string(value);
     }
--- source/value/value.d
+++ source/value/value.d
@@ -34,6 +34,11 @@ abstract class DatacheValue {
         }
     }
 
+    override string toString() {
+        return "<DatacheValue with no representation>";
+    }
+
+    /// by default, addition is not supported for the base class
     public DatacheValue add(DatacheValue other) {
         throw new TypeMismatchException("Cannot add types " ~ type ~ " and " ~ other.type);
     }
