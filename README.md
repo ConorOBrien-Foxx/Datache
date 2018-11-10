09343caf4f3dc25fd8a3e38cb7fe592784072a81
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-11-10 16:05:28 -0500

    initialize a functional test structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Datache
 
 A rewrite of Attache in D.
+
+Run tests as `dub test --main-file=test.d`
---
+++ test.d
@@ -0,0 +1,7 @@
+import std.stdio;
+
+int main() {
+    int a = 2 + 3;
+    assert(3 == a);
+    return 0;
+}
